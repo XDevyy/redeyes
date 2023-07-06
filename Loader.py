1037a2a9ce4c9f8aa4b6572de65b9b596f5a24aa
DevX <112074121+XDevyy@users.noreply.github.com>
2023-07-07 00:11:04 +0200

    Add files via upload

---
+++ Loader.py
@@ -0,0 +1,19 @@
+import time
+import random
+import string
+import os
+
+print("""
+  _____    ___         __  ____               
+ / ___ \  / _ \___ ___/ / / __/_ _____ ___    
+/ / _ `/ / , _/ -_) _  / / _// // / -_|_-<    
+\ \_,_/ /_/|_|\__/\_,_/ /___/\_, /\__/___/    
+ \___/                      /___/             
+
+""")
+
+print("         <=======Loading=======>")
+time.sleep(4)
+
+print("Hi, Select the tool you need to run.")
+time.sleep(3)
